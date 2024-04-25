7f9cdc0dfeabd81f9db1db0eaa6595ee5dac4352
Dongmo Russel Brandon <russeldongmo96@gmail.com>
2024-04-25 10:47:45 +0200

    projet bd

---
+++ src/main/java/com/discipline/Gestionemploidutemps/GestionEmploiDuTempsApplication.java
@@ -0,0 +1,13 @@
+package com.discipline.Gestionemploidutemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEmploiDuTempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEmploiDuTempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discipline/Gestionemploidutemps/GestionEmploiDuTempsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discipline.Gestionemploidutemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEmploiDuTempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
